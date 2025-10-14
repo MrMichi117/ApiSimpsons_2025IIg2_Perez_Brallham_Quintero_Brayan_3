bff15c5db5707b5f9614a1402518c5c46d925414
BrayanOrtiz115 <brayortiz78@gmail.com>
2025-10-14 17:28:20 -0500

    Api Simpson

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/ApiSimpsons_2025IIg2_Perez_Brallham_Quintero_Brayan_3/'
+})
